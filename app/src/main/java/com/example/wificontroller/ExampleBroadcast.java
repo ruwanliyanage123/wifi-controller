3f70b549b1e38461c67ca665ed4b83ce936703f5
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-12-06 21:59:34 +0530

    Add user permissions

---
+++ app/src/main/java/com/example/wificontroller/ExampleBroadcast.java
@@ -0,0 +1,15 @@
+package com.example.wificontroller;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.widget.Toast;
+
+public class ExampleBroadcast extends BroadcastReceiver {
+
+    }
+}
