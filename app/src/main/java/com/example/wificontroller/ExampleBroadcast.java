8dad35fd0e16da6b4ff823b1379b63405eff8f32
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-12-06 22:01:01 +0530

    Implement onReceive method

--- app/src/main/java/com/example/wificontroller/ExampleBroadcast.java
+++ app/src/main/java/com/example/wificontroller/ExampleBroadcast.java
@@ -11,5 +11,26 @@ import android.widget.Toast;
 
 public class ExampleBroadcast extends BroadcastReceiver {
 
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        WifiManager wifiManager;
+        String connectedSsid;
+        if("android.net.conn.CONNECTIVITY_CHANGE".equals(intent.getAction())){
+            NetworkInfo netInfo = intent.getParcelableExtra (WifiManager.EXTRA_NETWORK_INFO);
+            if (ConnectivityManager.TYPE_WIFI == netInfo.getType ()) {
+                wifiManager = (WifiManager) context.getSystemService (Context.WIFI_SERVICE);
+                WifiInfo info = wifiManager.getConnectionInfo ();
+                connectedSsid = info.getSSID();
+                String authorizedSsidToCompare = "\"ruwan\"";
+                if(!connectedSsid.equals(authorizedSsidToCompare)){
+                    wifiManager.disconnect();
+                    if(connectedSsid!=null) {
+                        Toast.makeText(context, "Disconnected! " + connectedSsid + "is not an authorized wifi network", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(context,"Connected to authorized wifi network",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
